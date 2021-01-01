a44109354d59ff28b85cf6892d2f632fd6b6bad7
tristanpendergrass <tristanpendergrass@gmail.com>
2020-12-31 16:43:13 -0800

    Add type variables to NetworkModel

--- src/NetworkModel.elm
+++ src/NetworkModel.elm
@@ -4,15 +4,15 @@ import Game
 import Lamdera exposing (ClientId)
 
 
-type alias NetworkModel =
+type alias NetworkModel gameModel gameMsg =
     { clientId : ClientId -- client id of this frontend
-    , serverSnapshot : Game.Model -- what this frontend thinks the server thinks is this model
-    , localMsgs : List Game.Msg -- updates to the model that have been sent to the server but not confirmed
-    , localModel : Game.Model -- serverModel with localMsgs applied
+    , serverSnapshot : gameModel -- what this frontend thinks the server thinks is this model
+    , localMsgs : List gameMsg -- updates to the model that have been sent to the server but not confirmed
+    , localModel : gameModel -- serverModel with localMsgs applied
     }
 
 
-init : ClientId -> Game.Model -> NetworkModel
+init : ClientId -> gameModel -> NetworkModel gameModel gameMsg
 init clientId initialModel =
     { clientId = clientId
     , serverSnapshot = initialModel
